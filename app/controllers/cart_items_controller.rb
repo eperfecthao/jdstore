75d43d39cf39248fd8342539449c8a7ef5a958fa
songhao <songhao@songhaodeMBP.lan>
2017-07-11 07:36:21 +0800

    can modify product quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,4 +7,18 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     redirect_to :back, alert: "成功将#{@product.title}删除!"
   end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to :back
+    end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
