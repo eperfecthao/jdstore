c7ead9691e66137b445d5d356779cb50831e33a7
songhao <songhao@songhaodeMBP.lan>
2017-07-10 22:50:57 +0800

    repeat product can't join shopping_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,14 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    redirect_to :back, notice: "加入购物车成功"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
+    else
+       flash[:alert] = "你已经将此商品加入过购物车了!"
+    end
+    redirect_to :back
   end
+
+
 end
