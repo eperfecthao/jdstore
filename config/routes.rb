c494711d3cabdb25219666757ac825bc654012c6
songhao <songhao@songhaodeMBP.lan>
2017-07-11 16:35:52 +0800

    add payment_method

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,12 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
-  resources :orders
+  resources :orders do
+    member do
+      post :payment_with_alipay
+      post :payment_with_wechat
+    end
+  end
 
   namespace :account do
     resources :orders
